59a8852b601fa82619a7f64bc7df096bf4ad8fae
Okabe <sb981335043@163.com>
2022-03-01 20:55:28 +0800

    2

--- SLIC.cpp
+++ SLIC.cpp
@@ -1,33 +1,151 @@
-// SLIC.cpp: implementation of the SLIC class.
-//===========================================================================
-// This code implements the zero parameter superpixel segmentation technique
-// described in:
-//
-//
-//
-// "SLIC Superpixels Compared to State-of-the-art Superpixel Methods"
-//
-// Radhakrishna Achanta, Appu Shaji, Kevin Smith, Aurelien Lucchi, Pascal Fua,
-// and Sabine Susstrunk,
-//
-// IEEE TPAMI, Volume 34, Issue 11, Pages 2274-2282, November 2012.
-//
-// https://www.epfl.ch/labs/ivrl/research/slic-superpixels/
-//===========================================================================
-// Copyright (c) 2013 Radhakrishna Achanta.
-//
-// For commercial use please contact the author:
-//
-// Email: firstname.lastname@epfl.ch
-//===========================================================================
-
 #include <stdio.h>
 #include <cfloat>
 #include <cmath>
 #include <iostream>
 #include <fstream>
-#include "SLIC.h"
 #include <chrono>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class SLIC
+{
+public:
+	SLIC();
+	virtual ~SLIC();
+
+	//============================================================================
+	// Superpixel segmentation for a given number of superpixels	输入
+	//============================================================================
+	void PerformSLICO_ForGivenK(
+		const unsigned int *ubuff, //Each 32 bit unsigned int contains ARGB pixel values.
+		const int width,
+		const int height,
+		int *klabels,
+		int &numlabels,
+		const int &K,
+		const double &m);
+
+	//============================================================================
+	// Save superpixel labels to pgm in raster scan order	保存聚类标记
+	//============================================================================
+	void SaveSuperpixelLabels2PPM(
+		char *filename,
+		int *labels,
+		const int width,
+		const int height);
+
+private:
+	//============================================================================
+	// Magic SLIC. No need to set M (compactness factor) and S (step size). #两种模式
+	// SLICO (SLIC Zero) varies only M dynamicaly, not S.
+	//============================================================================
+	void PerformSuperpixelSegmentation_VariableSandM(
+		vector<double> &kseedsl,
+		vector<double> &kseedsa,
+		vector<double> &kseedsb,
+		vector<double> &kseedsx,
+		vector<double> &kseedsy,
+		int *klabels,
+		const int &STEP,
+		const int &NUMITR);
+
+	//============================================================================
+	// Pick seeds for superpixels when number of superpixels is input.
+	//============================================================================
+	void GetLABXYSeeds_ForGivenK(
+		vector<double> &kseedsl,
+		vector<double> &kseedsa,
+		vector<double> &kseedsb,
+		vector<double> &kseedsx,
+		vector<double> &kseedsy,
+		const int &STEP,
+		const bool &perturbseeds,
+		const vector<double> &edges);
+
+	//============================================================================
+	// Move the seeds to low gradient positions to avoid putting seeds at region boundaries.
+	//============================================================================
+	void PerturbSeeds(
+		vector<double> &kseedsl,
+		vector<double> &kseedsa,
+		vector<double> &kseedsb,
+		vector<double> &kseedsx,
+		vector<double> &kseedsy,
+		const vector<double> &edges);
+
+	//============================================================================
+	// Detect color edges, to help PerturbSeeds()
+	//============================================================================
+	void DetectLabEdges(
+		const double *lvec,
+		const double *avec,
+		const double *bvec,
+		const int &width,
+		const int &height,
+		vector<double> &edges);
+
+	//============================================================================
+	// xRGB to XYZ conversion; helper for RGB2LAB()
+	//============================================================================
+	void RGB2XYZ(
+		const int &sR,
+		const int &sG,
+		const int &sB,
+		double &X,
+		double &Y,
+		double &Z);
+
+	//============================================================================
+	// sRGB to CIELAB conversion
+	//============================================================================
+	void RGB2LAB(
+		const int &sR,
+		const int &sG,
+		const int &sB,
+		double &lval,
+		double &aval,
+		double &bval, double *&tableRGB);
+
+	//============================================================================
+	// sRGB to CIELAB conversion for 2-D images
+	//============================================================================
+	void DoRGBtoLABConversion(
+		const unsigned int *&ubuff,
+		double *&lvec,
+		double *&avec,
+		double *&bvec);
+
+	//============================================================================
+	// Post-processing of SLIC segmentation, to avoid stray labels.
+	//============================================================================
+	void EnforceLabelConnectivity(
+		const int *labels,
+		const int &width,
+		const int &height,
+		int *nlabels,	//input labels that need to be corrected to remove stray labels
+		int &numlabels, //the number of labels changes in the end if segments are removed
+		const int &K);	//the number of superpixels desired by the user
+
+private:
+	int m_width;
+	int m_height;
+	int m_depth;
+
+	double *m_lvec;
+	double *m_avec;
+	double *m_bvec;
+
+	double **m_lvecvec;
+	double **m_avecvec;
+	double **m_bvecvec;
+};
+
+
+
+
+
 
 typedef chrono::high_resolution_clock Clock;
 
@@ -86,43 +204,6 @@ SLIC::~SLIC()
 	}
 }
 
-//==============================================================================
-///	RGB2XYZ
-///
-/// sRGB (D65 illuninant assumption) to XYZ conversion
-//==============================================================================
-/* 直接注释掉 我
-void SLIC::RGB2XYZ(
-	const int &sR,
-	const int &sG,
-	const int &sB,
-	double &X,
-	double &Y,
-	double &Z)
-{
-	double R = sR / 255.0;
-	double G = sG / 255.0;
-	double B = sB / 255.0;
-
-	double r, g, b;
-
-	if (R <= 0.04045)
-		r = R / 12.92;
-	else
-		r = pow((R + 0.055) / 1.055, 2.4);
-	if (G <= 0.04045)
-		g = G / 12.92;
-	else
-		g = pow((G + 0.055) / 1.055, 2.4);
-	if (B <= 0.04045)
-		b = B / 12.92;
-	else
-		b = pow((B + 0.055) / 1.055, 2.4);
-
-	X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
-	Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
-	Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
-}*/
 
 void SLIC::RGB2LAB(const int &sR, const int &sG, const int &sB, double &lval, double &aval, double &bval, double *&tableRGB)
 {
@@ -141,34 +222,6 @@ void SLIC::RGB2LAB(const int &sR, const int &sG, const int &sB, double &lval, do
     Y = r0 * 0.2126729 + g0 * 0.7151522 + b0 * 0.0721750;
     Z = r0 * 0.0193339 + g0 * 0.1191920 + b0 * 0.9503041;
 
-	//------------------------
-	// XYZ to LAB conversion
-	//------------------------
-/*
-	double epsilon = 0.008856; //actual CIE standard
-	double kappa = 903.3;	   //actual CIE standard
-
-	double Xr = 0.950456; //reference white
-	double Yr = 1.0;	  //reference white
-	double Zr = 1.088754; //reference white
-
-	double xr = X / Xr;
-	double yr = Y / Yr;
-	double zr = Z / Zr;
-
-	if (xr > epsilon)
-		fx = pow(xr, 1.0 / 3.0);
-	else
-		fx = (kappa * xr + 16.0) / 116.0;
-	if (yr > epsilon)
-		fy = pow(yr, 1.0 / 3.0);
-	else
-		fy = (kappa * yr + 16.0) / 116.0;
-	if (zr > epsilon)
-		fz = pow(zr, 1.0 / 3.0);
-	else
-		fz = (kappa * zr + 16.0) / 116.0;
-*/
     double fx, fy, fz;
     if (X > 0.008417238336) fx = cbrt(X * (1.0 / 0.950456));
     else fx = (8.192982069151272 * X + 0.1379310344827586);
